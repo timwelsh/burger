0b2b7f41003d3580ef31d83bd7286176913960d2
Tim Welsh <tim@timwelsh.com>
2019-03-20 11:30:18 -0400

    first files

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const router = express.Router();
+const burger = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  burger.all(function(data) {
+    const hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/api/burger", function(req, res) {
+  burger.create([
+    "name", "sleepy"
+  ], [
+    req.body.name, req.body.sleepy
+  ], function(result) {
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
+  });
+});
+
+router.put("/api/burger/:id", function(req, res) {
+  const condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.update({
+    sleepy: req.body.sleepy
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+router.delete("/api/burger/:id", function(req, res) {
+  const condition = "id = " + req.params.id;
+
+  burger.delete(condition, function(result) {
+    if (result.affectedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+use burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Erma', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Max', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', false);
+
+
+select * from burgers
